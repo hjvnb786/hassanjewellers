c17aace94972555cef86a68ec09efa92227eac49
tuaha1 <tuahakst@gmail.com>
2025-07-27 12:42:07 +0300

    Fix spacing in UserDetailsForm - remove extra bottom padding

--- lib/Widgets/join_scheme_widgets/user_details_form.dart
+++ lib/Widgets/join_scheme_widgets/user_details_form.dart
@@ -579,7 +579,6 @@ class _UserDetailsFormState extends State<UserDetailsForm> {
                 ),
               ),
             ),
-            const SizedBox(height: 24),
           ],
         ),
       ),
